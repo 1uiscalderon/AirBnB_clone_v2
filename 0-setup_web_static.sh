e5726ad80ba248ee2edd689f33c15aa5df85246b
1uiscalderon <1857@holbertonschool.com>
2020-12-22 13:27:06 -0500

    sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+apt-get update
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Testing" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "/listen 80 default_server;/a \ \tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
+service nginx restart
