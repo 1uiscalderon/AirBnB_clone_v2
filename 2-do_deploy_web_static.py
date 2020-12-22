35abcdc25452cb6414cde287c4ee26491351eb80
1uiscalderon <1857@holbertonschool.com>
2020-12-22 17:48:52 -0500

    Second task

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Fabric script"""
+from fabric.api import put, run, env
+from os.path import exists
+import os
+env.user = 'ubuntu'
+env.hosts = ['35.196.95.205', '34.73.121.54']
+
+
+def do_deploy(archive_path):
+    """Deploy new static content"""
+    if not os.path.isfile(archive_path):
+        return False
+    try:
+        upload = put(archive_path, "/tmp/")
+        name = upload[0].split("/")[2].split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{}.tgz -C "
+            "/data/web_static/releases/{}".format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} "
+            "/data/web_static/current".format(name))
+        return True
+    except Exception:
+        return False
