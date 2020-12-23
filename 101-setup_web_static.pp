3170da29e5ecfae7ca7176a2842088ccebc02d1d
1uiscalderon <1857@holbertonschool.com>
2020-12-22 22:59:26 -0500

    Changed sudo teee for redirection

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,18 +3,19 @@
 package { 'nginx':
   ensure => installed,
 }
-exec { 'Setup':
-  command  => 'sudo mkdir -p /date/web_static/releases/test
-  sudo mkdir -p /data/web_static/shared
-  sudo echo "Testing" > /data/web_static/releases/test/index.html
-  sudo ln -sf /data/web_static/releases/test /data/web_static/current
-  sudo chown -R ubuntu:ubuntu /data/
-  sed -i "/listen 80 default_server;/a \ \tlocation \/hbnb_static {\n\t\talias 
+exec { 'configuration':
+  command  => '
+  mkdir -p /data/web_static/releases/test
+  mkdir -p /data/web_static/shared
+  echo "Testing" > /data/web_static/releases/test/index.html
+  ln -sf /data/web_static/releases/test /data/web_static/current
+  chown -R ubuntu:ubuntu /data/
+  sed -i "/listen 80 default_server;/a \ \tlocation \/hbnb_static {\n\t\talias
   \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
-  sudo service nginx restart',
+  service nginx restart',
   provider => 'shell'
 }
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx'],
+  require => package['nginx']
 }
