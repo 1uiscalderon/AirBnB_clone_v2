bf43fa9a62f6b77ee934d6105742741a072bacd4
1uiscalderon <1857@holbertonschool.com>
2020-12-22 22:45:59 -0500

    Advanced task

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,20 @@
+# Configure an Nginx server using Puppet instead of Bash
+
+package { 'nginx':
+  ensure => installed,
+}
+exec {
+  command  => 'sudo mkdir -p /date/web_static/releases/test
+  sudo mkdir -p /data/web_static/shared
+  sudo echo "Testing" | sudo tee /data/web_static/releases/test/index.html
+  sudo ln -sf /data/web_static/releases/test /data/web_static/current
+  sudo chown -R ubuntu:ubuntu /data/
+  sed -i "/listen 80 default_server;/a \ \tlocation \/hbnb_static {\n\t\talias 
+  \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
+  sudo service nginx restart',
+  provider => 'shell'
+}
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
