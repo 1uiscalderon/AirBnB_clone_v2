a2dcef891afa89db86ab08321a83aed18170f7db
1uiscalderon <1857@holbertonschool.com>
2021-01-19 15:31:13 -0500

    Fixing None type error

--- models/state.py
+++ models/state.py
@@ -2,10 +2,12 @@
 """ State Module for HBNB project for AirBNB_clone_v2"""
 
 
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.engine.file_storage import FileStorage
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -13,13 +15,14 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state')
-
-    @property
-    def cities(self):
-        """Getter attribute cities that returns the list of City instances"""
-        from models import storage
-        new_list = []
-        for key, obj_city in storage.all(City).items():
-            if obj_city.state_id == self.id:
-                new_list.append(obj_city)
-        return new_list
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter attribute cities that returns a list of City instance"""
+            from models import storage
+            from models.city import City
+            new_list = []
+            for key, obj_city in storage.all(City).items():
+                if obj_city.state_id == self.id:
+                    new_list.append(obj_city)
+            return new_list
